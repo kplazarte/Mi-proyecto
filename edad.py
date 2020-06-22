c8d68366989ad6f580631369585c846195903d08
Kevin Plazarte <kplazarte@est.ups.edu.ec>
2020-06-22 12:20:04 -0500

    Commit inicial

---
+++ edad.py
@@ -0,0 +1,10 @@
+# Codigo de ejemplo para Git
+# Autor: Kevin Plazarte <kplazarte@est.ups.edu.ec>
+# Fecha: 22-06-2020
+# Version 0.0.1
+# Calcular la edad del año actual
+anio = 2020
+nombre = input("Cual es tu nombre? ")
+anio_nacimiento = int(input("En que año naciste: "))
+edad = anio - anio_nacimiento
+print("\nHola" , nombre , "tu edad en este año sera:", edad); 
